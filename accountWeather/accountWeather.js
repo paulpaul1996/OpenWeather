aa6a6bf9243353dca560046344ba075b6b401cdc
paulpaul1996 <42634142+paulpaul1996@users.noreply.github.com>
2022-01-26 17:52:56 +0200

    Added batch class and lwc component

---
+++ accountWeather/accountWeather.js
@@ -0,0 +1,48 @@
+import { LightningElement, api, wire } from 'lwc';
+// import { getRecord } from 'lightning/uiRecordApi';
+// import NAME_FIELD from '@salesforce/schema/Account.Name';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+import BILLING_ADDRESS from '@salesforce/schema/Account.BillingAddress';
+// import CURRENT_WEATHER from '@salesforce/schema/Account.AccountCurrentWeather__c';
+// import BILLING_LAT from '@salesforce/schema/Account.BillingLongitude';
+// import BILLING_LONG from '@salesforce/schema/Account.BillingLatitude';
+
+export default class AccountWeather extends LightningElement {
+    @api recordId;
+    @wire(getRecord, { recordId: '$recordId', fields: ['Account.BillingCity','Account.BillingCountry','Account.AccountCurrentWeather__c', 'Account.BillingLatitude','Account.BillingLongitude']})
+    account;
+    weather;
+
+    get billingAddress() {
+        return getFieldValue(this.account.data, 'Account.BillingCity') + ' ' + getFieldValue(this.account.data, 'Account.BillingCountry');
+    }
+
+    get currentWeather() {
+        this.weather = getFieldValue(this.account.data, 'Account.AccountCurrentWeather__c');
+    }
+
+    get billingLatitude(){
+        return getFieldValue(this.account.data, 'Account.BillingLatitude');
+    }
+    
+    get billingLongitude() {
+        return getFieldValue(this.account.data, 'Account.BillingLongitude');
+    }
+
+    
+    renderedCallback() {
+        refreshApex(this.account);
+        if(this.billingLongitude && this.billingLatitude) {
+            let endPoint = "https://api.openweathermap.org/data/2.5/onecall?lat=" + this.billingLatitude +"&lon=" + this.billingLongitude + "&appid=09a93c50ae11be4f195dbb9a9d1caff5";
+            console.log(endPoint);
+            fetch(endPoint)
+            .then((response) => {
+                response.json().then(result => this.weather = result.current.weather[0].description);
+            })
+        } else {
+            this.weather = 'Invalid latitude and longitude';
+        }
+    }
+}
